8938fef3abf9de95f2ae63f6c43a99412f835eed
hiba-bacha <hibabacha30@gmail.com>
2026-01-16 17:33:12 +0100

    Delete COM-project directory

--- COM-project/run/errors.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-@dataclass(frozen=True)
-class SyntaxErrorInfo:
-    line: int          # line number starting at 1
-    message: str       # human readable error message
-    text: str          # original line text
